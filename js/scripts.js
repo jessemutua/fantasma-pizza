740d1908c5957e17db6a95009c5a61e5411b2786
Jesse <jessemutua76@gmail.com>
2021-06-25 16:47:41 +0300

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -63,29 +63,34 @@ $(document).ready(function() {
                         } else if (crust === "flat-bed") {
                             sumPrice = (price * number) + 60;
                         } else if (crust === "thick") + 70;
+
+                        break;
+                    case size = "medium":
+                        price = 500;
+                        if (crust === "thin") {
+                            sumPrice = (price * number) + 30;
+                        } else if (crust === "custom") {
+                            sumPrice = (price * number) + 50;
+                        } else if (crust === "flay-bed") {
+                            sumPrice = (price * number) + 60;
+                        } else if (crust === "thick") {
+                            sumPrice = (price * number) + 70;
+                        }
+                        break;
+                    case size = "large":
+                        price = 500;
+                        if (crust === "thin") {
+                            sumPrice = (price * number) + 30;
+                        } else if (crust === "custom") {
+                            sumPrice = (price * number) + 50;
+                        } else if (crust === "flat-bed") {
+                            sumPrice = (price * number) + 60;
+                        } else if (crust === "thick") {
+                            sumPrice = (price * number) + 70;
+                        }
+                        break;
                 }
-                break;
-            case size = "medium":
-                price = 500;
-                if (crust === "thin") {
-                    sumPrice = (price * number) + 30;
-                } else if (crust === "custom") {
-                    sumPrice = (price * number) + 50;
-                } else if (crust === "flay-bed") {
-                    sumPrice = (price * number) + 60;
-                } else if (crust === "thick") {
-                    sumPrice = (price * number) + 70;
-                }
-                break;
-            case size = "large":
-                price = 500;
-                if (crust === "thin") {
-                    sumPrice = (price * number) + 30;
-                } else if (crust === "custom") {
-                    sumPrice = (price * number) + 50;
-                } else if (crust === "flat-bed") {
-                    sumPrice = (price * number) + 60;
-                } else if (crust === "thick") + 70
+
         }
 
     })
